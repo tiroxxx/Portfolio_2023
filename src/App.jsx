67c9c00452c4d40033021b3f63a84cf4bf94909e
tiroxxx <christianmarquez41@gmail.com>
2023-11-21 19:34:47 -0500

    added navbar and hero components to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+
 function App() {
 
   return (
-    <div>
-      3D developer portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
