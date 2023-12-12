3e4c6c934f436ea532087b4f2bbc43f38f40dffc
tiroxxx <christianmarquez41@gmail.com>
2023-12-11 19:42:03 -0500

    added configurations for section wrapper to animate each section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Tilt } from "react-tilt"
-import { motion } from "framer-motion"
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from "../styles"
-import { services } from '../constants'
-import { fadeIn, textVariant } from "../utils/motion"
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+import { SectionWrapper } from "../hoc";
 
 function ServiceCard({ index, title, icon }) {
   return (
@@ -12,21 +14,24 @@ function ServiceCard({ index, title, icon }) {
       options={{
         max: 30,
         scale: 1,
-        speed: 450
+        speed: 450,
       }}
-      className="xs:w-[250px] w-full">
-      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-        >
-          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
-  )
+  );
 }
-
 
 const About = () => {
   return (
@@ -35,21 +40,23 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] mas-w-3xl leading-[30px]'>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] mas-w-3xl leading-[30px]"
+      >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
