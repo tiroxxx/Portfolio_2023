389e78886c62d1fa8d1028d3aaeebe7f602706d3
tiroxxx <christianmarquez41@gmail.com>
2023-12-14 18:13:15 -0500

    added info to the timeline

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -30,7 +30,24 @@ const ExperienceCard = ({ experience }) => (
   >
     <div>
       <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p
+        className="text-secondary text-[16px] font-semibold"
+        style={{ margin: 0 }}
+      >
+        {experience.company_name}
+      </p>
     </div>
+    <ul className="mt-5 list-dics ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li
+          style={{ listStyleType: "circle" }}
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 );
 
